2b277ef4dc666ff2d939877383908f759bd860a4
Asuka Hongo <araaiko.122@gmail.com>
2022-05-24 18:05:01 +0900

    buttonスタイルの追加

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-// import { styled } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 
 
-// const UseStylesButton = styled(Button)({
-//     color: '#000',
-//     backgroundColor: 'green',
-//     transition: 'all 0.3s ease',
-//     '&:hover': {
-//         backgroundColor: '#fff',
-//     },
-// });
+const MyStylesButton = styled(Button)({
+    color: '#FFB549',
+    borderColor: '#FFB549',
+    fontWeight: 600,
+    transition: 'all 0.3s ease',
+    marginBottom: '8px',
+    '&:hover': {
+        backgroundColor: '#FFB549',
+        color: '#fff'
+    },
+});
 
 const Answer = (props) => {
     return (
         <>
-            {/* <UseStylesButton variant="contained">Test</UseStylesButton> */}
-            <Button variant="contained" onClick={() => {props.select(props.content, props.nextId)}}>
+            <MyStylesButton variant="outlined" onClick={() => {props.select(props.content, props.nextId)}}>
                 {props.content}
-            </Button>
+            </MyStylesButton>
         </>
     );
 }
